3c2aa1d961198a9dabd50cd12ade69680c946819
Chih-Chuan Chan <dincoco@gmail.com>
2018-04-29 09:18:16 -0400

    Tuning learning rate.

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         for images, labels in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
                 feed_dict={input_image: images, correct_label: labels,
-                           keep_prob: 0.8, learning_rate: 0.0001})
+                           keep_prob: 0.5, learning_rate: 0.0001})
             print("Loss: {}".format(loss))
 tests.test_train_nn(train_nn)
 
